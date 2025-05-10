b291ab6f3eb4508e9aebf9e865e7073618678138
User Name <mckylesingh71@gmail.com>
2025-05-10 13:20:42 +0200

    Employees can Add farmer profiles

--- Controllers/FarmersController.cs
+++ Controllers/FarmersController.cs
@@ -1,4 +1,6 @@
 ﻿using Agri_Connect.Data;
+using Agri_Connect.Models.Entities;
+using Agri_Connect.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +13,60 @@ namespace Agri_Connect.Controllers
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ApplicationDbContext _context;
 
+        public FarmersController(UserManager<IdentityUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        // POST: Farmer/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterFarmerViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, "Farmer");
+
+                    var farmer = new Farmer
+                    {
+                        FullName = model.FullName,
+                        FarmName = model.FarmName,
+                        Location = model.Location,
+                        UserId = user.Id
+                    };
+
+                    _context.Farmers.Add(farmer);
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction("Index", "Home"); // Or another admin page
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
         }
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 
 namespace Agri_Connect.Controllers
 {
+    [Authorize(Roles = "Farmer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
