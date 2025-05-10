9a59a89d61969b00c7c6a86b1e78e1869dc34419
User Name <mckylesingh71@gmail.com>
2025-05-10 10:50:57 +0200

    Added Roles

---
+++ Data/SeedData.cs
@@ -0,0 +1,26 @@
+﻿using Agri_Connect.Models;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Agri_Connect.Data
+{
+    public static class SeedData
+    {
+        public static async Task InitializeAsync(IServiceProvider serviceProvider)
+        {
+
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var roles = new[] { "Employee", "Farmer", "User" };
+
+            // Create roles if they don't exist
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
