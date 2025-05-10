67d7f58628f3217a1de3fd0f5157dea6cfe7fee1
User Name <mckylesingh71@gmail.com>
2025-05-10 15:21:00 +0200

    Created products controller and viewModel

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Agri_Connect.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/ViewModels/AddProductViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Agri_Connect.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Agri_Connect.Models.ViewModels
+{
+    public class AddProductViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Required]
+        [Display(Name = "Product Type")]
+        public ProductType Type { get; set; }
+
+        [Display(Name = "Image URL")]
+        public string? ProductImageUrl { get; set; } // Stored image URL in DB
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public int FarmerId { get; set; }  // This can b
+    }
+}
